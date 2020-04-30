ebab90e547a98dde6426fe054db1bdf56f08462c
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-04-29 23:36:12 -0500

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,18 @@
+# Create Security Group for EC2
+resource "aws_security_group" "default" {
+  name = "terraform-default-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

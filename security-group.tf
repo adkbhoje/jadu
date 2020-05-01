ae331b46b6665c5d6743f54b3f62611a881dec28
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-01 12:32:48 -0500

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -16,35 +16,7 @@ provider "aws" {
   secret_key = "${var.secretKey}"
   region     = "us-east-2"
 }
-# Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = var.ami
-  count                  = var.instance_count
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.default.id]
-  source_dest_check      = false
-  instance_type          = var.instance_type
-
-  tags = {
-    Name = "terraform-default"
-  }
+resource "aws_instance" "example" {
+  ami           = "ami-0520e698dd500b1d1"
+  instance_type = "t2.micro"
 }
-
-# Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  }
